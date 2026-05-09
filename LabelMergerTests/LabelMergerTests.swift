a297f810bfbad86e702e83b078bcb628f0ea1b5f
Karl David Garcia <karldavid323@gmail.com>
2026-05-09 10:44:46 +0800

    Initial Commit

---
+++ LabelMergerTests/LabelMergerTests.swift
@@ -0,0 +1,17 @@
+//
+//  LabelMergerTests.swift
+//  LabelMergerTests
+//
+//  Created by Karl Garcia on 5/9/26.
+//
+
+import Testing
+@testable import LabelMerger
+
+struct LabelMergerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
